96fb3a6603b9b11573147dfd48f816be800f722d
Carlos Guagrilla <carlos.guagrilla@hotmail.com>
2021-06-28 11:30:58 -0500

    router

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+//traemos el paquete
+const { Router } = require('express');
+const data = require("../Data.json")
+//iniciamos el route
+const router = Router()
+
+// crecion de los endpoints
+router
+    .get("/", (req, res)=>{
+        
+    })
